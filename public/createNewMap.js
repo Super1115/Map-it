0f466ef30940a0b857148c90aefabc1bc82672cb
Super <chuangsuper@gmail.com>
2024-10-05 17:40:11 +0800

    add map

---
+++ public/createNewMap.js
@@ -0,0 +1,26 @@
+function newMapTestButton(){
+    let titleForTest = prompt("Title");
+    newMap(titleForTest)
+}
+
+function newMap(title){
+    const database = firebase.database();
+    const user = firebase.auth().currentUser;
+    const UID = user.uid;
+    const userName = user.displayName
+    const newMap = {
+        title: title,
+        user: userName,
+        UID: UID,
+        time: Date.now() 
+    };
+    database.ref('/maps/').push(newMap)
+    .then(snapshot => {
+        console.log('Map added');
+        //應導向渲染地圖
+    })
+    .catch(error => {
+        console.error('Error adding object:', error);
+    });
+}
+
